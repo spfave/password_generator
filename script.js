ffc5cd0d626b8323ad069b3b1f3f3f5ad062c82d
Sebastian F <sebpfave2@gmail.com>
2021-03-03 20:30:48 -0500

    Return list of pw char type from configPwChar func

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ const lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const numbers = "012346789".split("");
 const symbols = "`~!@#$%^&*()-_=+[]{}\\|;:'\",.<>/?".split("");
 
+let pwCharType = [];
+
 // Functions
 // Configure password generation inputs
 function configPassword() {
-  let includeLettersLower, includeLettersUpper, includeNumbers, includeSymbols;
-
-  configPasswordCharacters();
+  const pwCharTypes = configPasswordCharacters();
+  console.log(pwCharTypes); // testing
 }
 
 function configPasswordCharacters() {
   // Select character types to include
-  includeLettersLower = confirm("Include lower case letters in your password?");
-  includeLettersUpper = confirm("Include upper case letters in your password?");
-  includeNumbers = confirm("Include numbers in your password?");
-  includeSymbols = confirm("Include symbols in your password?");
+  const includeLettersLower = confirm(
+    "Include lower case letters in your password?"
+  );
+  const includeLettersUpper = confirm(
+    "Include upper case letters in your password?"
+  );
+  const includeNumbers = confirm("Include numbers in your password?");
+  const includeSymbols = confirm("Include symbols in your password?");
 
   // Confirm selection of at least 1 character type
   if (
@@ -29,8 +34,24 @@ function configPasswordCharacters() {
     alert(
       "Must select at least 1 password character type to generate a password."
     );
-    configPasswordCharacters();
+    pwCharType = configPasswordCharacters();
+  } else {
+    pwCharType = [];
+    if (includeLettersLower) {
+      pwCharType.push("l");
+    }
+    if (includeLettersUpper) {
+      pwCharType.push("u");
+    }
+    if (includeNumbers) {
+      pwCharType.push("n");
+    }
+    if (includeSymbols) {
+      pwCharType.push("s");
+    }
   }
+
+  return pwCharType;
 }
 
 // Write password to the #password input
