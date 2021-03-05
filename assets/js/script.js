f108ffaf742d41b3331ca70ee77c962221342802
Sebastian F <sebpfave2@gmail.com>
2021-03-04 23:16:48 -0500

    Edit code comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,16 @@
-// Set available password characters
-const lettersLower = "abcdefghijklmnopqrstuvwxyz".split("");
-const lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-const numbers = "012346789".split("");
-const symbols = "`~!@#$%^&*()-_=+[]{}\\|;:'\",.<>/?".split("");
-
+// Define character list object with array of characters for each character type
 const charLists = {
-  l: "abcdefghijklmnopqrstuvwxyz".split(""),
-  u: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
-  n: "012346789".split(""),
-  s: "`~!@#$%^&*()-_=+[]{}\\|;:'\",.<>/?".split(""),
+  l: "abcdefghijklmnopqrstuvwxyz".split(""), // lowercase letters
+  u: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""), // uppercase letters
+  n: "012346789".split(""), // numbers
+  s: "`~!@#$%^&*()-_=+[]{}\\|;:'\",.<>/?".split(""), // symbols
 };
 
-// let pwCharType = [];
-// let pwLength;
-
-// Functions
+// FUNCTIONS
 function generatePassword() {
   // Configure password generation inputs
   const pwCharTypes = configPasswordCharacters();
   const pwLength = configPasswordLength();
-  // console.log(pwCharTypes); //test
-  // console.log(pwLength); //test
 
   // Generate password character set
   return generatePasswordText(pwCharTypes, pwLength);
@@ -36,6 +26,7 @@ function configPasswordCharacters() {
   ];
   let pwCharTypes = [];
 
+  // Prompt for each character type to include in password
   for (const charType of charTypes) {
     const includeCharSet = confirm(`Include ${charType.desc} in password`);
     if (includeCharSet) {
@@ -43,9 +34,9 @@ function configPasswordCharacters() {
     }
   }
 
-  /* 
-  Confirm selection of at least 1 character type 
-  - if no character types selected: alert user and rerun character type selection prompts
+  /**
+   * Confirm selection of at least 1 character type
+   * if no character types selected: alert user and rerun character type selection prompts
    */
   if (pwCharTypes.length === 0) {
     alert(
@@ -59,12 +50,13 @@ function configPasswordCharacters() {
 
 // Input password character length
 function configPasswordLength() {
+  // Promp for password length
   const pwLengthInput = prompt("Enter password length");
   const isWNum = /^\d+$/.test(pwLengthInput); // Evaluate if input is only digits (whole number)
 
-  /* 
-  Confirm password length input is whole number between [8, 128]
-  - if input is not a whole number or input number is not between [8, 128]: alert user and rerun password length input prompt
+  /**
+   * Confirm password length input is whole number between [8, 128]
+   * if input is not a whole number or input number is not between [8, 128]: alert user and rerun password length input prompt
    */
   let pwLength = Number(pwLengthInput);
   if (!isWNum || pwLength < 8 || pwLength > 128) {
@@ -77,29 +69,29 @@ function configPasswordLength() {
 
 // generatePasswordText();
 function generatePasswordText(pwCharTypes, pwLen) {
-  // console.log(pwCharTypes);
-  // console.log(shuffleArray(pwCharTypes));
-  let pwText = [];
+  const pwText = [];
 
-  /*  */
+  /**
+   * To include at least one character of each character type selected to use in password
+   * loop over character type selection list and randomly assign a character of that type to the password character set
+   */
   for (const pwCharType of pwCharTypes) {
     const rChar = randomChoice(charLists[pwCharType]);
     pwText.push(rChar);
   }
-  console.log(pwText); //test
 
-  /*  */
+  /**
+   * To select characters to fill the password character set to the password length
+   * loop over the remaining spaces to fill and randomly select a selected character type and a character of that type
+   */
   for (let c = pwCharTypes.length; c < pwLen; c++) {
-    // const element = array[c];
     const rCharType = randomChoice(pwCharTypes);
     const rChar = randomChoice(charLists[rCharType]);
     pwText.push(rChar);
   }
-  console.log(pwText); //test
 
-  // Shuffle password character array and return
+  // Shuffle password character array and return as joined string
   return shuffleArray(pwText).join("");
-  // return "test";
 }
 
 function randomChoice(array) {
@@ -108,8 +100,10 @@ function randomChoice(array) {
 }
 
 function shuffleArray(array) {
-  // Return random shuffle of array: uses Durstenfeld shuffle (Fisher-Yates) algorithm
-  // Credit: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  /**
+   * Return random shuffle of array: uses Durstenfeld shuffle (Fisher-Yates) algorithm
+   * Credit: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+   */
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -125,9 +119,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// DOM Control
-// Assignment Code
-const generateBtn = document.querySelector("#generate");
-
+// DOM CONTROL
 // Add event listener to generate button
+const generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
